228f1b9801fe974f14938d7e8765990f64c60d84
milording <milording@gmail.com>
2017-12-15 21:39:07 +0300

    skeleton

---
+++ Balalaika/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Balalaika
+//
+//  Created by milording on 13/12/2017.
+//  Copyright © 2017 milording. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
