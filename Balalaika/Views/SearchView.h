bd8dcbd6a289612ab0abddb02537a067139bef0d
milording <milording@gmail.com>
2018-01-08 06:24:55 +0300

    view

---
+++ Balalaika/Views/SearchView.h
@@ -0,0 +1,13 @@
+//
+//  SearchView.h
+//  Balalaika
+//
+//  Created by milording on 08/01/2018.
+//  Copyright © 2018 milording. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchView : UIViewController
+
+@end
---
+++ Balalaika/Views/SearchView.m
@@ -0,0 +1,22 @@
+//
+//  SearchView.m
+//  Balalaika
+//
+//  Created by milording on 08/01/2018.
+//  Copyright © 2018 milording. All rights reserved.
+//
+
+#import "SearchView.h"
+
+@interface SearchView ()
+
+@end
+
+@implementation SearchView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
